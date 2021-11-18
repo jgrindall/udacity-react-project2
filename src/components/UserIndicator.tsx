034b44f2907aac6cc78c00dd3d11b248e09461e8
jgrindall <john.grindall@gmail.com>
2021-11-18 19:08:41 +0000

    add UserIndicator

---
+++ src/components/UserIndicator.tsx
@@ -0,0 +1,70 @@
+import React, {Component} from 'react'
+import {connect, ConnectedProps} from "react-redux";
+import {Link, RouteComponentProps} from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import {RootState} from "../types";
+import {setAuthedUser} from "../actions/authedUser";
+
+const mapStateToProps = (state: RootState) => {
+    return {
+        loading: !state.authedUser,
+        authedUser:state.authedUser,
+        quizUsers:state.quizUsers
+    }
+};
+
+const connector = connect(mapStateToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+type MyProps = PropsFromRedux & RouteComponentProps & {
+    navProp: any
+};
+
+type MyState = {
+
+};
+
+class UserIndicator extends Component<MyProps, MyState> {
+    onLogout(){
+       this.props.dispatch(setAuthedUser(null));
+    }
+    render() {
+        const user = this.props.authedUser ? this.props.quizUsers[this.props.authedUser] : null;
+        const button = user ?
+            (
+                <div>
+
+                    <img src={user.avatarURL}/>
+                    <span>
+                        {user.name}
+                    </span>
+                    <button
+                        onClick={this.onLogout.bind(this)}
+                        className="pure-button pure-button-primary"
+                    >
+                    Logout
+                    </button>
+                </div>
+            )
+            :
+            (
+                <div>
+                    <span>
+                        You are not logged in
+                    </span>
+                    <Link to={{
+                        pathname: '/login'
+                    }}>Login</Link>
+                </div>
+            );
+        return (
+            <div className='user'>
+                {button}
+            </div>
+        );
+    }
+}
+
+export default withRouter(connector(UserIndicator));
+
